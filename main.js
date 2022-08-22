927d0584e77959928188e3663274ed0fbe97ac49
ptlmeet27 <20124040@nuv.ac.in>
2022-08-23 01:27:28 +0530

    add data to dom fn

--- main.js
+++ main.js
@@ -115,13 +115,26 @@ const displayweather = (data) =>{
     console.log(data.weather.description);
     console.log(data.weather[0].main);
 
+    let temp = document.querySelector(".temp");
+    let weatherdet = document.querySelector(".weatherdetails");
+    let feelslike = document.querySelector(".feels");
+    let humidity = document.querySelector(".humidity");
+    let visibility = document.querySelector(".visibility");
+    let wind = document.querySelector(".wind");
+
+    temp.textContent = `${Math.round(data.main.temp-273.15)} °C`;
+    weatherdet.textContent=`${data.weather[0].main}`;
+    feelslike.textContent= `Feels Like : ${Math.round(data.main.feels_like - 273.15)} °C`;
+    humidity.textContent=`Humidity : ${data.main.humidity} %`;
+    visibility.textContent=`Visibility : ${data.visibility / 1000} Km`;
+    wind.textContent=`Wind Speed : ${Math.round((data.wind.speed * 60 * 60) / 1000)} Km/h`;
 }
 
 const updateImg = (data) =>{
     const container = document.getElementById("cont");
     let weather = data.weather[0].main;
 
-    if(weather === "Rain" || weather === "Drizzle"){
+    if(weather === "Rain" || weather === "Drizzle" || weather === "Thunderstorm"){
         container.classList.add("rain");
         container.classList.remove("clear");
         container.classList.remove("haze");
